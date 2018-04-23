d3e05bfdf3100cd8d19faab20783df1fb3ff83ae
Sebastian Hajdus <sebastian.hajdus@gmail.com>
2018-04-23 21:53:50 +0200

    nav menu: text position, auto hide nav

--- src/js/script.js
+++ src/js/script.js
@@ -1,4 +1,21 @@
 $(function() {
 	console.log('ready!');
 	
+	var c, currentScrollTop = 0,
+	navbar = $('nav');
+
+	$(window).scroll(function () {
+		var a = $(window).scrollTop();
+		var b = navbar.height();
+
+		currentScrollTop = a;
+
+		if (c < currentScrollTop && a > b + b) {
+			navbar.addClass("scrollUp");
+		} else if (c > currentScrollTop && !(a <= b)) {
+			navbar.removeClass("scrollUp");
+		}
+		  c = currentScrollTop;
+	});
+
 });
